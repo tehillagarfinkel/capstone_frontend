3f64ff0fe4c31437fdca1c3881183a65e588c373
Tehilla Garfinkel <tehillagarfinkel613@gmail.com>
2019-08-04 16:06:30 -0500

    add categories new/create action

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
+    <h1>New Category</h1>
+    Name:
+    <input v-model="categoryName" type="text" />
+    Image:
+    <input v-model="categoryImage" type="text" />
+    <button v-on:click="createCategory()">Create Category</button>
     <h1>All Categories</h1>
     <div v-for="category in categories">
       <img v-bind:src="category.image" alt="category.name" />
@@ -18,7 +24,9 @@ export default {
   data: function() {
     return {
       message: "Welcome to Vue.js!",
-      categories: []
+      categories: [],
+      categoryName: "",
+      categoryImage: ""
     };
   },
   created: function() {
@@ -27,6 +35,19 @@ export default {
       this.categories = response.data;
     });
   },
-  methods: {}
+  methods: {
+    createCategory: function() {
+      var params = {
+        name: this.categoryName,
+        image: this.categoryImage
+      };
+      axios.post("/api/categories", params).then(response => {
+        console.log(response.data);
+        this.categories.push(response.data);
+        this.categoryName = "";
+        this.categoryImage = "";
+      });
+    }
+  }
 };
 </script>
