be68e55705dff97529a1dbc96609bb7093ec6b75
Tehilla Garfinkel <tehillagarfinkel613@gmail.com>
2019-08-04 15:45:24 -0500

    add categories index action

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,32 @@
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
+    <h1>All Categories</h1>
+    <div v-for="category in categories">
+      <img v-bind:src="category.image" alt="category.name" />
+      <h2>{{ category.name }}</h2>
+    </div>
   </div>
 </template>
 
 <style></style>
 
 <script>
+import axios from "axios";
+
 export default {
   data: function() {
     return {
-      message: "Welcome to Vue.js!"
+      message: "Welcome to Vue.js!",
+      categories: []
     };
   },
-  created: function() {},
+  created: function() {
+    axios.get("/api/categories").then(response => {
+      console.log(response.data);
+      this.categories = response.data;
+    });
+  },
   methods: {}
 };
 </script>
