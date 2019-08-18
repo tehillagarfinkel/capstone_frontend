eebb5952714af9831a179cee1c7008fed9e287a0
Tehilla Garfinkel <tehillagarfinkel613@gmail.com>
2019-08-18 16:41:15 -0500

    update task create action to include params for all details, incorporate theme with task create action

--- src/views/CategoryShow.vue
+++ src/views/CategoryShow.vue
@@ -45,130 +45,62 @@
               </div>
             </div>
           </div>
-          <!--  <div class="col-lg-3 col-sm-6 col-xs-12">
-            <div class="pricingTable">
-              <div class="priceUper">
-                <div class="priceTitle bg-color-2"><h3>New Task</h3></div>
-                <div class="priceInfo bg-color-2">
-                  <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots.</p>
-                </div>
-              </div>
-              <div class="priceLower">
-                <ul class="list-unstyled priceOffer">
-                  <li>
-                    <i class="fa fa-birthday-cake color-2" aria-hidden="true"></i>
-                    2 Meals/Day
-                  </li>
-                  <li>
-                    <i class="fa fa-medkit color-2" aria-hidden="true"></i>
-                    Medical Assistance
-                  </li>
-                  <li>
-                    <i class="fa fa-paint-brush color-2" aria-hidden="true"></i>
-                    Drowing &amp; Dancing
-                  </li>
-                  <li>
-                    <i class="fa fa-taxi color-2" aria-hidden="true"></i>
-                    Free Transport
-                  </li>
-                </ul>
-                <div class="priceBtn">
-                  <a href="checkout-step-1.html" class="btn btn-primary bg-color-2">Choose Plan</a>
-                  <h4 class="color-2">
-                    $69
-                    <br />
-                    <small>Per Month</small>
-                  </h4>
-                </div>
-              </div>
-            </div>
-          </div> -->
-          <!--  <div class="col-lg-3 col-sm-6 col-xs-12">
-            <div class="pricingTable">
-              <div class="priceUper">
-                <div class="priceTitle bg-color-3"><h3>Fullday Session</h3></div>
-                <div class="priceInfo bg-color-3">
-                  <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots.</p>
-                </div>
-              </div>
-              <div class="priceLower">
-                <ul class="list-unstyled priceOffer">
-                  <li>
-                    <i class="fa fa-birthday-cake color-3" aria-hidden="true"></i>
-                    2 Meals/Day
-                  </li>
-                  <li>
-                    <i class="fa fa-medkit color-3" aria-hidden="true"></i>
-                    Medical Assistance
-                  </li>
-                  <li>
-                    <i class="fa fa-paint-brush color-3" aria-hidden="true"></i>
-                    Drowing &amp; Dancing
-                  </li>
-                  <li>
-                    <i class="fa fa-taxi color-3" aria-hidden="true"></i>
-                    Free Transport
-                  </li>
-                </ul>
-                <div class="priceBtn">
-                  <a href="checkout-step-1.html" class="btn btn-primary bg-color-3">Choose Plan</a>
-                  <h4 class="color-3">
-                    $69
-                    <br />
-                    <small>Per Month</small>
-                  </h4>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="col-lg-3 col-sm-6 col-xs-12">
-            <div class="pricingTable">
-              <div class="priceUper">
-                <div class="priceTitle bg-color-4"><h3>Weekly Session</h3></div>
-                <div class="priceInfo bg-color-4">
-                  <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots.</p>
-                </div>
-              </div>
-              <div class="priceLower">
-                <ul class="list-unstyled priceOffer">
-                  <li>
-                    <i class="fa fa-birthday-cake color-4" aria-hidden="true"></i>
-                    2 Meals/Day
-                  </li>
-                  <li>
-                    <i class="fa fa-medkit color-4" aria-hidden="true"></i>
-                    Medical Assistance
-                  </li>
-                  <li>
-                    <i class="fa fa-paint-brush color-4" aria-hidden="true"></i>
-                    Drowing &amp; Dancing
-                  </li>
-                  <li>
-                    <i class="fa fa-taxi color-4" aria-hidden="true"></i>
-                    Free Transport
-                  </li>
-                </ul>
-                <div class="priceBtn">
-                  <a href="checkout-step-1.html" class="btn btn-primary bg-color-4">Choose Plan</a>
-                  <h4 class="color-4">
-                    $69
-                    <br />
-                    <small>Per Month</small>
-                  </h4>
-                </div>
-              </div>
-            </div>
-          </div> -->
         </div>
       </div>
     </section>
 
-    <div>
-      <h2>New Task</h2>
-      Description:
-      <input v-model="taskDescription" type="text" />
-      <button v-on:click="createTask">Create Task</button>
-    </div>
+    <section class="mainContent full-width clearfix">
+      <div class="container">
+        <div class="sectionTitle text-center">
+          <h2>
+            <span class="shape shape-left bg-color-4"></span>
+            <span>Create Task</span>
+            <span class="shape shape-right bg-color-4"></span>
+          </h2>
+        </div>
+
+        <div class="row 1">
+          <div class="col-lg-3 col-sm-6 col-xs-12">
+            <div class="pricingTable">
+              <div class="priceUper">
+                <div class="priceTitle bg-color-1">
+                  <h3>New Task</h3>
+                </div>
+              </div>
+              <div class="priceLower">
+                <ul class="list-unstyled priceOffer">
+                  <li>
+                    <i class="fa fa-taxi color-1" aria-hidden="true"></i>
+                    Description:
+                    <input v-model="taskDescription" type="text" />
+                  </li>
+                  <li>
+                    <i class="fa fa-birthday-cake color-1" aria-hidden="true"></i>
+                    Duration:
+                    <input v-model="taskDuration" type="text" />
+                  </li>
+                  <li>
+                    <i class="fa fa-medkit color-1" aria-hidden="true"></i>
+                    Due Date:
+                    <input v-model="taskDueDate" type="text" />
+                  </li>
+                  <li>
+                    <i class="fa fa-paint-brush color-1" aria-hidden="true"></i>
+                    Start time:
+                    <input v-model="taskStartTime" type="text" />
+                  </li>
+                </ul>
+                <div class="priceBtn">
+                  <div v-on:click="createTask" class="btn btn-primary bg-color-1">
+                    Create Task
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
     <div>
       Name:
       <input v-model="category.name" type="text" />
@@ -194,7 +126,10 @@ export default {
       tasks: [],
       name: "",
       image: "",
-      taskDescription: ""
+      taskDescription: "",
+      taskDuration: "",
+      taskDueDate: "",
+      taskStartTime: ""
     };
   },
   created: function() {
@@ -228,12 +163,18 @@ export default {
     createTask: function() {
       var params = {
         category_id: this.category.id,
-        description: this.taskDescription
+        description: this.taskDescription,
+        duration: this.taskDuration,
+        due_date: this.taskDueDate,
+        start_time: this.taskStartTime
       };
       axios.post("/api/tasks", params).then(response => {
         console.log(response.data);
         this.tasks.push(response.data);
         this.taskDescription = "";
+        this.taskDuration = "";
+        this.taskDueDate = "";
+        this.taskStartTime = "";
       });
     }
   }
