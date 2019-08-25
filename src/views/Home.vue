cf8a2bc0f2d8c9111b3a272d87de63ecb33db774
Tehilla Garfinkel <tehillagarfinkel613@gmail.com>
2019-08-25 10:53:09 -0500

    add links to home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="home">
+    <h1>Welcome to</h1>
     <img src="../assets/ON IT logo.png" alt="" />
+    <a href="/login" class="btn btn-primary btn-lg active" role="button" aria-pressed="true">Login</a>
+    <a href="signup" class="btn btn-primary btn-lg active" role="button" aria-pressed="true">Sign Up</a>
   </div>
 </template>
 
